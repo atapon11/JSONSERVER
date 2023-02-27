d418483977463068fa5b82ef4803d836997c5589
atapon11 <634259017@webmail.npru.ac.th>
2023-02-27 11:46:52 +0700

    Initial commit

---
+++ jsonserver/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as EmpCreate } from './EmpCreate'
+export { default as EmpDetail } from './EmpDetail'
+export { default as EmpEdit } from './EmpEdit'
+export { default as EmpList } from './EmpList'
